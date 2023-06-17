791634f27e3fe9059d49d6f94ea62693d874fcb9
kvbxss <kubadownar444@gmail.com>
2023-06-17 18:13:39 +0200

    made and atyled header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = () => {
+  return (
+    <Head>
+        <TitleWrapper>
+        <Title>
+            Dynamic Table
+        </Title>
+        </TitleWrapper>   
+    </Head>
+  )
+}
+
+export default Header
+
+const Head = styled.section`
+display: flex;
+justify-content: center;
+align-items: flex-start;
+`
+
+const TitleWrapper = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+
+`
+
+const Title = styled.h1`
+font-size: 42px;
+font-family: 'Mukta', sans-serif;
+color: #72b6b2;
+font-weight: 500;
+`
