18d509c9fb56ffd57965a7dd78139bae67e99054
kvbxss <kubadownar444@gmail.com>
2023-06-17 18:12:13 +0200

    passed proper components and props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header'
+import PokemonTable from './components/Table.tsx'
+import { styled } from 'styled-components'
+import Options from './components/Options'
+import Footer from './components/Footer'
 
-function App() {
+const App = () => {
+  const [selectedPokemon, setSelectedPokemon] = useState('');
+
+  const handleRowClick = (navigate, pokemonName) => {
+    const newPath = `/Pokemons/${pokemonName}`;
+    setSelectedPokemon(pokemonName);
+    navigate(newPath);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+    <Header /> 
+    <Wrapper>
+      <Options />
+      <StickyWrapper></StickyWrapper>
+      <PokemonTable selectedPokemon={selectedPokemon} handleRowClick={handleRowClick}/>
+    </Wrapper>
+    <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
+
+
+
+const Wrapper = styled.section`
+  display: block;
+  position: relative;
+  overflow: hidden;
+  background-color: #f5f5f5;
+  border-radius: 0.375rem;
+  --tw-shadow: 0 4px 6px -1px rgb(0 0 0 / .1), 0 2px 4px -2px rgb(0 0 0 / .1);
+  box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+
+  @media (min-width: 768px) {
+    height: calc(100vh - 215px);
+  }
+`
+
+const StickyWrapper = styled.div`
+  position: sticky;
+  top: 50px;
+  overflow: hidden;
+`
