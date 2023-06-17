1de473cf633710b6d8d92e0131a5d683499375c0
kvbxss <kubadownar444@gmail.com>
2023-06-17 18:12:43 +0200

    created footer and styled it

---
+++ src/components/Footer.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import heartIcon from '../assets/heart.svg'
+
+const Footer = () => {
+  return (
+    <Foot>
+      <Text>
+        Developed with 
+      </Text>
+      <Heart>
+        <HeartIcon src={heartIcon}/>
+      </Heart>
+      <Text>
+        by <a href="jakubdownarowicz.pl">Jakub Downarowicz</a>
+      </Text>
+    </Foot>
+  )
+}
+
+export default Footer;
+
+const Foot = styled.div`
+display: flex;
+--tw-text-opacity: 1;
+color: rgba(82, 82, 82, var(--tw-text-opacity));
+font-family: 'Mukta', sans-serif;
+font-size: 1rem;
+line-height: 1.25rem;
+margin-top: 0.5rem;
+`
+
+const Text = styled.p`
+font-family: 'Mukta', sans-serif;
+font-size: 1rem;
+line-height: 1.25rem;
+
+
+`
+
+const Heart = styled.span`
+  padding: 0 0.5rem;
+`
+
+const HeartIcon = styled.img`
+width: 1rem;
+height: 1rem;
+margin-top: 1rem;
+`
