81048280ee973cf1852a1b328b965bbcb4ef8f53
kvbxss <kubadownar444@gmail.com>
2023-06-17 18:10:28 +0200

    connection to rapidapi - fetched data from rapidapi and made interfaces to carry data

---
+++ src/services/Api.tsx
@@ -0,0 +1,21 @@
+export async function getPokemons() {
+    try {
+        const response = await fetch("https://pokemon-go1.p.rapidapi.com/pokemon_stats.json", {
+          method: 'GET',
+          headers: {
+            'X-RapidAPI-Key': '44d0c99e07msh095b1fe1715e5a1p111afcjsnd20a39ea3dff',
+            'X-RapidAPI-Host': 'pokemon-go1.p.rapidapi.com'
+          }
+        });
+      
+        if (response.ok) {
+          const result = await response.json();
+          return result;
+          
+        }
+      } catch (err) {
+        console.error(err);
+      }
+      return [];
+}
+
---
+++ src/services/interfaces.tsx
@@ -0,0 +1,14 @@
+export interface PokemonStats {
+    base_attack:  number;
+    base_defense: number;
+    base_stamina: number;
+    form:         string;
+    pokemon_id:   number;
+    pokemon_name: string;
+   }
+
+export interface PokemonStatsResponse {
+    pokemons: PokemonStats[];
+}
+
+   
